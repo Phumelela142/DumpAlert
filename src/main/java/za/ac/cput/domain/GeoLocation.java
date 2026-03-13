68ace7af386e7edde601a81641913c650c89ab64
IngaPlati <230126634@mycput.ac.za>
2026-03-14 01:18:47 +0200

    Added GeoLocation and PickUpSchedule entities

--- src/main/java/za/ac/cput/domain/GeoLocation.java
+++ src/main/java/za/ac/cput/domain/GeoLocation.java
@@ -1,4 +1,23 @@
 package za.ac.cput.domain;
 
 public class GeoLocation {
+
+    private double latitude;
+    private double longitude;
+
+    public GeoLocation (double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+    public String getLocation(){
+        return "Latitude: " + latitude + ", Longitude: " + longitude;
+    }
 }
